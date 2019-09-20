c80deb2db2d1d354fbc0bfab815aca8bb76fa20f
dheemanth-gowda <dheethu12@gmail.com>
2019-09-20 22:27:00 +0530

    added demojs

---
+++ src/components/demo.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class extends Component {
+    state = {}
+    render() {
+        return (  );
+    }
+}
+
+import React, { Component } from 'react';export de
+fault;
