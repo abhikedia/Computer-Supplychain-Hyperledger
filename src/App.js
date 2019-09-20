3e184b099648a2d3211159dd14465b11ec8adeff
dheemanth-gowda <dheethu12@gmail.com>
2019-09-21 01:49:14 +0530

    removed unwanted lines

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-react';
 import Navbar from '../src/components/navbar'
-import Userpage from '../src/pages/usersVerify'
 import UserV from '../src/pages/userV'
 import Maps from '../src/pages/maps'
 
