c45acb682a833d2288a11a8d43f6a749be1a0de9
dheemanth-gowda <dheethu12@gmail.com>
2019-09-21 05:40:13 +0530

    added checkpoint

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
 import UserV from '../src/pages/userV'
 import Maps from '../src/pages/maps'
 import Manufacturer from '../src/pages/manufacturer'
+import Checkpoint from '../src/pages/checkpoints'
 import * as serviceWorker from './serviceWorker';
 
+
 const routing = (
-    <Router>
-      <div>
-        <Route exact path="/user" component={UserV} />
-        <Route exact path="/maps" component={Maps} />
-        <Route exact path="/manufacturer" component={Manufacturer} />
-      </div>
-    </Router>
-  )
+  <Router>
+    <div>
+      <Route exact path="/user" component={UserV} />
+      <Route exact path="/maps" component={Maps} />
+      <Route exact path="/manufacturer" component={Manufacturer} />
+      <Route exact path="/checkpoints" component={Checkpoint} />
+    </div>
+  </Router>
+)
 
 
 ReactDOM.render(routing, document.getElementById('root'));
